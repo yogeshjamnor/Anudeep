65968445a91ba331ad84eb4f9fa7b0aac9057995
yogeshjamnor <63215819+yogeshjamnor@users.noreply.github.com>
2024-03-26 17:27:51 +0530

    Update MonthDay.java

--- MonthDay.java
+++ MonthDay.java
@@ -2,14 +2,14 @@ import java.time.Month;
 public class MonthDay 
 {
 public static void main(String[] args)
-		{
-		// Loop through all months in the enum
-		for (Month month : Month.values()) 
-			{
-			// Print the month name and total number of days
-			// Using Month.length(boolean leapYear) to get the total number of days in the month
-			// Passing false to ensure it doesn't consider a leap year
-			System.out.println(month.toString() + ": " + month.length(false) + " days");
-			}
-		}
+   {
+	 // Loop through all months in the enum
+	for (Month month : Month.values()) 
+	{
+	  // Print the month name and total number of days
+	  // Using Month.length(boolean leapYear) to get the total number of days in the month
+	  // Passing false to ensure it doesn't consider a leap year
+	  System.out.println(month.toString() + ": " + month.length(false) + " days");
+	}
+    }
 }
