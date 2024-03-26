f8f81541287eed934cffeec36b9d2c62a7d62dd8
yogeshjamnor <63215819+yogeshjamnor@users.noreply.github.com>
2024-03-26 17:44:13 +0530

    Update InputHandle.java

--- InputHandle.java
+++ InputHandle.java
@@ -1,3 +1,4 @@
+//take integer as input and display input mismatch if user enter without number
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class InputHandle 
@@ -15,7 +16,7 @@ public static void main(String[] args)
 			// If no exception occurs, display the entered integer
 			System.out.println("You entered: " + number);
 			// Ask the user if they want to enter another integer
-					System.out.print("Do you want to enter another integer? (y/n): ");
+			System.out.print("Do you want to enter another integer? (y/n): ");
 			String choice = scanner.next();
 			if (!choice.equalsIgnoreCase("y")) 
 			{
